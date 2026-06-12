8b6c91b66f2462060bac41bb203befdedd776ec4
Sergii Vdovareize <servdo@gmail.com>
2026-06-12 23:55:24 +0300

    test: add unit test for MainContainer component rendering MemeField

---
+++ src/components/MainContainer.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import MainContainer from './MainContainer';
+
+jest.mock('./MemeField', () => () => <div data-testid="mock-meme-field">Meme Field</div>);
+
+describe('MainContainer Component', () => {
+    test('renders MemeField component', () => {
+        render(<MainContainer />);
+        expect(screen.getByTestId('mock-meme-field')).toBeInTheDocument();
+    });
+});
