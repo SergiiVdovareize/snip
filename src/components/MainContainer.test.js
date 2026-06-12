1d1daa4cba3835b9578fbf3668fe743fc8fe36da
Sergii Vdovareize <servdo@gmail.com>
2026-06-12 23:56:56 +0300

    refactor: reformat MemeField mock for improved readability in MainContainer tests

--- src/components/MainContainer.test.js
+++ src/components/MainContainer.test.js
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import MainContainer from './MainContainer';
 
-jest.mock('./MemeField', () => () => <div data-testid="mock-meme-field">Meme Field</div>);
+jest.mock('./MemeField', () => () => (
+    <div data-testid="mock-meme-field">Meme Field</div>
+));
 
 describe('MainContainer Component', () => {
     test('renders MemeField component', () => {
